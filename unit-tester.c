3105c2d8a7f8b3373f27d8e77ca7153c28a6db6d
yongqian10 <yongqian@live.com.my>
2018-03-09 14:53:05 +0000

    add small code snippet to test mem address mod

--- unit-tester.c
+++ unit-tester.c
@@ -4,7 +4,16 @@
 #include <unistd.h>
 #include <sys/mman.h>
 
-#define memfile = "/dev/mem";
-
-int fd = open("memfile", (O_RDWR | O_SYNC));
+int main(){
+int fd = open("/dev/mem", (O_RDWR | O_SYNC));
 void* virtual_base = mmap(NULL, 0x04000000, (PROT_READ | PROT_WRITE), MAP_SHARED, fd, 0x00 );
+// aligned base based on offset
+void* aligned_base = virtual_base + 0x21000;
+
+
+// small test snippet
+void* a = (void*)0x10;
+void* intptr =  (void*)a;
+void* inptrsum =  intptr + 0x10;
+printf("%d", &inptrsum);
+}
